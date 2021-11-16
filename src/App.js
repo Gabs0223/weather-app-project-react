52bf3a322e9fe0e097da43238ecfb3f4a9373185
Gabs0223 <sanchezgabriela23@gmail.com>
2021-11-15 21:08:54 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>weatherApp</h1>
+      This project was coded by Gabri Sanchez and is{" "}
+      <a
+        href="https://github.com/Gabs0223/weather-project-react"
+        target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+    </div>
+  );
+}
+
+export default App;
